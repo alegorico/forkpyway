ca4440fc9b946d3728f563d310d054658a19b06e
Alejandro G. <mierdeneitor@gmail.com>
2025-10-14 13:50:03 +0200

    Add pytest marker for Oracle factory test

--- tests/test_oracle.py
+++ tests/test_oracle.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from __future__ import annotations
+import pytest
 
 from pyway.dbms.database import factory
 from pyway.dbms import oracle
 
 
+@pytest.mark.oracle_test
 def test_oracle_factory() -> None:
     """Test that the Oracle database adapter can be loaded by the factory."""
     cls = factory('oracle')
